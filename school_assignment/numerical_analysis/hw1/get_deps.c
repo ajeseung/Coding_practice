0d84f211449af0092068089de1326fc9579db643
ajeseung <145750431+ajeseung@users.noreply.github.com>
2024-11-25 17:33:37 +0900

    Delete school_assignment directory

--- school_assignment/numerical_analysis/hw1/get_deps.c
+++
@@ -1,8 +0,0 @@
-double get_deps() {
-	double deps = 1;
-	do {
-		deps /= 2;
-	} while (deps + 1 > 1);
-	
-	return deps*2;
-}
--- school_assignment/numerical_analysis/hw1/get_feps.c
+++
@@ -1,8 +0,0 @@
-float get_feps() {
-	float feps = 1;
-	do {
-		feps /= 2;
-	} while (feps + 1 > 1);
-
-	return feps*2;
-}
